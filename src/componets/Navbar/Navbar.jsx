5f2f2ec3d7a530bedbc2b7cd9dd250afa5d3b80f
AkininVladimir <akinin_vv@mail.ru>
2022-03-19 19:22:03 +0300

    use NavLink

--- src/componets/Navbar/Navbar.jsx
+++ src/componets/Navbar/Navbar.jsx
@@ -1,26 +1,27 @@
 import React from 'react';
 import classes from './Navbar.module.css';
+import {NavLink} from "react-router-dom";
 
 const Navbar = () => {
     return (
         <nav className={classes.Navbar}>
-            <div className={`${classes.item} ${classes.active}`}>
-                <a href="#">Профиль</a>
+            <div className={classes.item}>
+                <NavLink to="/Profile" className={(navData) => navData.isActive ? classes.active: classes.item} >Профиль</NavLink>
             </div>
             <div className={classes.item}>
-                <a href="#">Диалоги</a>
+                <NavLink to='/Dialog'className={(navData) => navData.isActive ? classes.active: classes.item}>Диалоги</NavLink>
             </div>
             <div className={classes.item}>
-                <a href="#">Сервисы</a>
+                <NavLink to="/Services"className={(navData) => navData.isActive ? classes.active: classes.item}>Сервисы</NavLink>
             </div>
             <div className={classes.item}>
-                <a href="#">Моя музыка</a>
+                <NavLink to="/Music"className={(navData) => navData.isActive ? classes.active: classes.item}>Моя музыка</NavLink>
             </div>
             <div className={classes.item}>
-                <a href="#">Настройки</a>
+                <NavLink to="/Settings"className={(navData) => navData.isActive ? classes.active: classes.item}>Настройки</NavLink>
             </div>
         </nav>
 
-    );
+    )
 }
 export default Navbar;
