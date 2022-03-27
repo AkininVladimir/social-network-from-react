33241ca6ae933c0bdadd2698c65d97cf962540f7
AkininVladimir <akinin_vv@mail.ru>
2022-03-27 15:45:17 +0300

    using array method - map

--- src/componets/Dialog/Dialog.jsx
+++ src/componets/Dialog/Dialog.jsx
@@ -19,27 +19,53 @@ const MessageItems = (props) => {
     );
 }
 const Dialog = () => {
+
+    let dialogData = [
+        {id: 1, name: 'Андрей'},
+        {id: 2, name: 'Cветлана'},
+        {id: 3, name: 'Алексей'},
+        {id: 4, name: 'Инна'},
+        {id: 5, name: 'Василий'},
+        {id: 6, name: 'Юля'},
+    ]
+
+    let dialogElements =dialogData.map(d => <DialogItems name={d.name} id={d.id} />)
+    //     [
+    //     ,
+    //     <DialogItems name={dialogData[1].name} id={dialogData[1].id}/>,
+    //     <DialogItems name={dialogData[2].name} id={dialogData[2].id}/>,
+    //     <DialogItems name={dialogData[3].name} id={dialogData[3].id}/>,
+    //     <DialogItems name={dialogData[4].name} id={dialogData[4].id}/>,
+    //     <DialogItems name={dialogData[5].name} id={dialogData[5].id}/>
+    // ]
+
+    let massegeData = [
+        {id: 1, massege: 'Привет! Как Дела? Как твои уроки по Уроки React JS'},
+        {id: 2, massege: 'Я рад и мне очень весело'},
+        {id: 3, massege: 'Все круто!'}
+    ]
+
+    let massegeElements = massegeData.map (m => <MessageItems text={m.massege} id={m.id}/> )
+        /*[
+        <MessageItems text={massegeData[0].massege} id={massegeData[0].id}/>,
+        <MessageItems text={massegeData[1].massege} id={massegeData[1].id}/>,
+        <MessageItems text={massegeData[2].massege} id={massegeData[2].id}/>
+    ]*/
+
     return (
         <div className={classes.Dialog_wrapper}>
             <div className={classes.Header}>
                 <input type="text" placeholder="Поиск"></input>
             </div>
             <div className={classes.User_List}>
-                <DialogItems name="Андрей" id="1"/>
-                <DialogItems name="Светлана" id="2"/>
-                <DialogItems name="Алексей" id="3"/>
-                <DialogItems name="Инна" id="4"/>
-                <DialogItems name="Василий" id="5"/>
-                <DialogItems name="Юля" id="6"/>
+                {dialogElements}
+
             </div>
             <div className={classes.Message}>
-                <MessageItems text='Привет! Как Дела? Как тови уроки по Уроки React JS'/>
-                <MessageItems text='Привет! Мне очень нравятся эти занятия'/>
-                <MessageItems text='Вы тоже занимаетесь?'/>
-
+                {massegeElements}
             </div>
         </div>
 
-    )
+    );
 }
 export default Dialog;
